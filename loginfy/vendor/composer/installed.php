6ab38d92cd9394864bde34c96b56a5d1041ea7dd
Sean Murphy <sean@iamseanmurphy.com>
2025-03-29 13:18:40 +0000

    1.0.3.7

---
+++ loginfy/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'f80d4dec4968efb65fa48b2598180eed9a47bfad',
+        'name' => 'jeweltheme/loginfy',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'jeweltheme/loginfy' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'f80d4dec4968efb65fa48b2598180eed9a47bfad',
+            'dev_requirement' => false,
+        ),
+    ),
+);
